81a4a313d3878078658fe67ec7537d44b7a83b16
Administrador <Administrador@172.16.15.147>
2018-09-27 16:15:53 -0600

    ventas del dia miercoles

--- src/semanadiez/EjertiendaMatriz.java
+++ src/semanadiez/EjertiendaMatriz.java
@@ -9,10 +9,17 @@ import java.util.Scanner;
  */
 public class EjertiendaMatriz {
     public static void main(String[]ags){
-        final int dias=7;
-        final int sucursales=7;
+        final int dias=3;
+        final int sucursales=2;
+        
         float[][] ventas= new float[dias][sucursales];
         float suma=0;
+        float miercoles=0;
+        float promedio1=0;
+        float promedio3=0;
+        float promedio2=0;
+        float promedio4=0;
+        
         Scanner leer= new Scanner(System.in);
         
         for(int i=0; i<dias; i++)
@@ -22,6 +29,25 @@ public class EjertiendaMatriz {
                 System.out.println("ingrese la venta total para el dia"+ (i+1)+ "de la sucursal"+(j+1));
                 ventas[i][j]=leer.nextFloat();//guardando el valor de ventas
                 suma +=ventas[i][j];//incrementando el total
+                
+                if(i == 2)
+                {//ventas del dia mircoles
+                    miercoles= miercoles + ventas[i][j];
+                    promedio3=suma/ventas[i][j];
+                }
+                
+                if(i==0){
+                    promedio1=suma/ventas[i][j];
+                }
+                
+                if(i==1){
+                    promedio2=suma/ventas[i][j];
+                }
+                
+                if(i==3){
+                    promedio4=suma/ventas[i][j];
+                }
+                
             }
         }
         //imprimiendo el total de ventas
@@ -32,8 +58,14 @@ public class EjertiendaMatriz {
                 for(int j=0; j<sucursales; j++)
                 {
                     //imprimiendo valor por valor d la matiz
-                 System.out.println("el avlor de venta en el dia"+(i+1)+"para la sucursal"+(j+1)+"es:"+ventas[i][j]);
+                 System.out.println("el avlor de venta en el dia " + (i+1) + "para la sucursal " + (j+1)+  " es: \n "+ventas[i][j]);
                 }
             }
+            System.out.println("las ventas para el dia miercoles son de: $" +miercoles);
+            System.out.println("el promedio del dia lunes es de : $" +promedio1);
+            System.out.println("el promedio del dia martes es de : $" +promedio2);
+            System.out.println("el promedio del dia miercoles es de : $" +promedio3);
+            System.out.println("el promedio del dia jueves es de : $" +promedio4);
+            
     }
 }
